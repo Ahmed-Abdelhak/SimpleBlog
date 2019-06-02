ac54c75e2a87a91336af8a7bf20e71a7f6624a54
EngineerAhmedMo <w-rl@hotmail.com>
2019-06-03 01:01:34 +0200

    list Articles at HomePage

--- SimpleBlog/Controllers/HomeController.cs
+++ SimpleBlog/Controllers/HomeController.cs
@@ -1,18 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.Models;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
     public class HomeController : BaseController
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _context.Dispose();
+        }
+
+
         public ActionResult Index()
         {
-
-            return View();
+            var articles = new ArticleViewModel()
+            {
+                Articles = _context.Articles.Include(a=>a.Category).Include(a=>a.Publisher).OrderByDescending(a=>a.DatePublished).ToList()
+            };
+            return View(articles);
         }
+
+
 
         public ActionResult Contact()
         {
